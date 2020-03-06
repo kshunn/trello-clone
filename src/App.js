9587090e9115c94b6e14e618f36e0a93c2c92ca4
kshunn <qkshunn@gmail.com>
2020-03-06 21:18:02 +0900

    fix: Apply custom fonts correctly

--- src/App.js
+++ src/App.js
@@ -2,19 +2,22 @@ import React from 'react';
 import { createGlobalStyle } from "styled-components";
 import Home from './routes/Home';
 
-createGlobalStyle`
+const GlobalStyle = createGlobalStyle`
     @import url('https://fonts.googleapis.com/css?family=Montserrat:500,700&display=swap');
     body{
       padding: 0;
       margin: 0;
       box-sizing: border-box;
+      font-family: 'Montserrat';
     }
-    font-family: Montserrat;
 `;
 
 function App() {
     return (
+      <>
+        <GlobalStyle />
         <Home />
+      </>
     );
 }
 
