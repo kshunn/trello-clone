16659e26ecc61753a64b4bc21ae22a832f1699b7
kshunn <qkshunn@gmail.com>
2020-03-06 22:21:57 +0900

    feat: Add HashRouter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import React from 'react';
+import { HashRouter, Route } from 'react-router-dom';
 import { createGlobalStyle } from "styled-components";
 import Home from './routes/Home';
+import BoardPage from './routes/BoardPage';
 
 const GlobalStyle = createGlobalStyle`
     @import url('https://fonts.googleapis.com/css?family=Montserrat:500,700&display=swap');
     body{
-      padding: 0;
-      margin: 0;
-      box-sizing: border-box;
-      font-family: 'Montserrat';
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+        font-family: 'Montserrat';
     }
 `;
 
 function App() {
     return (
-      <>
-        <GlobalStyle />
-        <Home />
-      </>
+        <>
+            <GlobalStyle />
+            <HashRouter>
+              <Route path="/" exact={true} component={Home} />
+              <Route path="/board/:id" component={BoardPage} />
+            </HashRouter>
+        </>
     );
 }
 
