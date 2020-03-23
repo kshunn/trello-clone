ad6d254033cba205631b3e66b15f8918997a5f8a
Sang Hun Kim <qkshunn@gmail.com>
2020-03-23 17:14:20 +0900

    feat: Show status and description in card page

--- src/components/CardBody.js
+++ src/components/CardBody.js
@@ -4,5 +4,24 @@ import { BoardListContext } from '../App';
 export default function CardBody({ boardKey, listKey, cardKey }){
   const { dispatch, findElement } = React.useContext(BoardListContext);
   const { done, description } = findElement(boardKey, listKey, cardKey);
-  return null;
+  const status = done ? 'Done' : 'Undone';
+  const oppositeStatus = done ? 'undone' : 'done';
+  return (
+    <div>
+      Status: {status}
+      <button
+        onClick={() => dispatch({
+          type: "TOGGLE_DONE",
+          payload: {
+            boardKey: boardKey,
+            listKey: listKey,
+            cardKey: cardKey
+          }
+        })}
+      >
+        Mark it as {oppositeStatus}
+      </button>
+      <p>{description}</p>
+    </div>
+  );
 }
